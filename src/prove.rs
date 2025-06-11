6a3cc2c04add98ddeb19636dc461b3908a0165f2
zkfriendly <zkfriendly@proton.me>
2025-06-11 09:35:49 +0200

    feat: add anyhow for error handling and implement generate_inputs function with tests

--- src/prove.rs
+++ src/prove.rs
@@ -4,11 +4,14 @@ use dotenv::dotenv;
 use reqwest::Client;
 use alloy::{primitives::Address};
 use relayer_utils::{generate_email_circuit_input, AccountCode, EmailCircuitParams, bytes32_to_fr};
-
+use anyhow::{Result, Context};
 pub async fn prove_handler(body: String) {
     println!("Request body: {}", body);
+}
+
+pub async fn generate_inputs(body: String) -> Result<String> {
     let random_bytes: [u8; 32] = [0; 32];
-    let fr = bytes32_to_fr(&random_bytes).unwrap();
+    let fr = bytes32_to_fr(&random_bytes)?;
     let account_code = AccountCode::from(fr);
     let params = EmailCircuitParams {
         ignore_body_hash_check: Some(false),
@@ -17,12 +20,27 @@ pub async fn prove_handler(body: String) {
         sha_precompute_selector: Some(String::from("(<div id=3D\"[^\"]*zkemail[^\"]*\"[^>]*>)")),
     };
 
-    let inputs = generate_email_circuit_input(&body, &account_code, Some(params)).await.unwrap();
-
-    println!("Generated inputs: {:?}", inputs);
-
+    let result = generate_email_circuit_input(&body, &account_code, Some(params))
+        .await
+        .context("Failed to generate email circuit inputs")?;
+    Ok(result)
 }
 
 pub fn routes() -> Router {
     Router::<()>::new().route("/", post(prove_handler))
+}
+
+#[cfg(test)]
+pub mod test {
+    use super::generate_inputs;
+    
+
+    #[tokio::test]
+    async fn test_generates_correct_inputs() {
+        let email = std::fs::read_to_string("test/fixtures/claim_ens_1/email.eml").unwrap();
+        let inputs = generate_inputs(email).await.unwrap();
+
+        println!("{}", inputs);
+        
+    }
 }
