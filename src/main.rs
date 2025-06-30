4e10ec97c5703610703175d33d913eb0e3ab714e
zkfriendly <zkfriendly@proton.me>
2025-06-30 10:21:50 +0200

    feat: allow all cors

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 use axum::Router;
 use dotenv::dotenv;
+use tower_http::cors::{Any, CorsLayer};
 mod claim;
 mod prove;
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
+
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_headers(Any);
+
     let app = Router::new()
         .nest("/claim", claim::routes())
-        .nest("/prove", prove::routes());
+        .nest("/prove", prove::routes())
+        .layer(cors);
     let listener = tokio::net::TcpListener::bind("0.0.0.0:4500").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
