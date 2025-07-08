c633646b3c01c09150b29748b3cbc191c6246e83
zkfriendly <zkfriendly@proton.me>
2025-07-08 11:13:11 +0200

    feat: generate proof based on email.eml

--- src/bin/fixtures.rs
+++ src/bin/fixtures.rs
@@ -1,8 +1,19 @@
-use ens_backend::prove::generate_inputs;
+use ens_backend::{prove::{generate_inputs, generate_proof}, state::StateConfig};
 
 #[tokio::main]
 async fn main() {
-    let email = std::fs::read_to_string("test/fixtures/case1_claim/email.eml").unwrap();
+    let config = StateConfig::from_file("config.json").expect("Could not load config file");
+    let basedir = std::path::PathBuf::from("test/fixtures/case1_claim");
+    // for each test case it is expected that the file basedir/email.eml already exists
+    // and then this script takes that file and generates inputs.json and prover_response.json in the same directory
+    let email_file_path = basedir.join("email.eml");
+
+    let email = std::fs::read_to_string(&email_file_path).unwrap();
     let inputs = generate_inputs(&email).await.unwrap();
-    println!("{:?}", inputs);
+    let proof = generate_proof(&email, &config.prover).await.expect("Could not generate proof");
+
+    
+
+    println!("{:?}", proof);
+   
 }
